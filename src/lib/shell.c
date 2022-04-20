1ef294e18e6f350d0354bc4303d74a7f40bf44bb
gbrlbrbs <gabrielbarbosamartinz_@outlook.com>
2022-04-20 15:44:55 -0300

    init shell with safer code

--- src/lib/shell.c
+++ src/lib/shell.c
@@ -1,5 +1,28 @@
 #include "shell.h"
 
-void init_shell() {
-    
+void init_shell(pid_t* shell_pgid, struct termios* shell_tmodes, int* shell_terminal, int* shell_is_interactive) {
+    *shell_terminal = STDIN_FILENO;
+    *shell_is_interactive = isatty(*shell_terminal);
+    if (*shell_is_interactive) {
+        do {
+            *shell_pgid = getpgrp();
+            kill(- (*shellpgid), SIGTTIN);
+        } while (tcgetpgrp(*shell_terminal) != *shell_pgid);
+
+        signal(SIGINT, SIG_IGN);
+        signal(SIGQUIT, SIG_IGN);
+        signal(SIGTSTP, SIG_IGN);
+        signal(SIGTTIN, SIG_IGN);
+        signal(SIGTTOU, SIG_IGN);
+        signal(SIGCHLD, SIG_IGN);
+
+        *shell_pgid = getpid();
+        if (setpgid(*shell_pgid, *shell_pgid) < 0) {
+            perror("Couldn't put the shell in its own process group.\n");
+            exit(1);
+        }
+
+        tcsetpgrp(*shell_terminal, *shell_pgid);
+        tcsetattr(*shell_terminal, shell_tmodes);
+    }
 }
--- src/lib/shell.h
+++ src/lib/shell.h
@@ -2,9 +2,13 @@
 #define SHELL_H
 
 #include <sys/types.h>
+#include <sys/signal.h>
+#include <sys/errno.h>
 #include <termios.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-void init_shell();
+void init_shell(pid_t* shell_pgid, struct termios shell_tmodes, int* shell_terminal, int* shell_is_interactive);
 
 #endif
